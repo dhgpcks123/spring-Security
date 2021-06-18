de7b2b515159d75c169f5bf188f008e8da9d6a4d
hyechan <dhgpcks@gmail.com>
2021-06-18 20:16:12 +0900

    security config chain 만들기

--- server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sp.fc.web.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/*
+@Order(1)
+    만약 두 개 이상의 필터체인을 구성하고 싶다면?
+    그냥 SecurityFilterChain을 하나 더 만들면 된다.
+    다만 필터 순서가 중요하기 때문에 @Order순서를 정해줘야한다.
+*/
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,11 +42,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 어떤 요청에 대해서 필터체인이 동작할 수 있게 설정하는 것!
+        http.antMatcher("/api/**");
+        // http 자체에 antMMatcher가 있다.
+        // 특정 api요청만 체크하겠다!
+
+        //필터 셋팅은 여기서!
         http.authorizeRequests((requests)->
                 requests.antMatchers("/").permitAll()
                         .anyRequest().authenticated()
         );
         http.formLogin();
         http.httpBasic();
+        /*
+        http
+                .headers().disable()
+                .csrf().disable()
+                .logout().disable()
+                .requestCache().disable()
+                ;
+                이 필터들 이제 작동 안 함-
+         */
     }
 }
